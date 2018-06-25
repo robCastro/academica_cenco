49f5f8810909523b2a07954d11cf16b371229606
Roberto Castro <roberto.ernesto.castro@gmail.com>
2018-06-24 21:21:41 -0600

    Grupos de Usuarios, Director cambiar contraseña, html base para grupos de usuarios.

--- apps_cenco/login/urls.py
+++ apps_cenco/login/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from apps_cenco.login import views
 
 urlpatterns = [
-    url(r'^director/$', views.directorCredencialesPropias),
+    url(r'^director/$', views.directorCredencialesPropias, name="director_credenciales_propias"),
+    url(r'^cerrar_sesion/$', views.log_out, name="cerrar_sesion"),
 ]
--- apps_cenco/login/views.py
+++ apps_cenco/login/views.py
@@ -1,9 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, update_session_auth_hash
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
 
+@login_required
 def directorCredencialesPropias(request):
-    return render(request, "login/director_credenciales_propias.html", {})
+    user = User.objects.get(username = request.user)
+    if user.groups.filter(name = "Director").exists():
+        mensaje = ""
+        clase_mensaje = ""
+        if request.method == "POST":
+            usuarioEscrito = request.POST.get('txtUsuario')
+            contraseniaActual = request.POST.get('vieja_contrasenia')
+            contraseniaNueva = request.POST.get('contrasenia')
+            if user.check_password(contraseniaActual):              #Validando contraseña
+                if user.get_username() != usuarioEscrito and User.objects.filter(username = usuarioEscrito).count() > 0:  #Validando usuario unico
+                    mensaje = "Usuario ya existe, escoger otro"
+                    clase_mensaje = "card border-warning mb-3"
+                else:
+                    user.username = usuarioEscrito
+                    user.set_password(contraseniaNueva)
+                    user.save()
+                    update_session_auth_hash(request, user)
+                    mensaje = "Cambios realizados"
+                    clase_mensaje = "card border-info mb-3"
+            else:
+                mensaje = "Contraseña incorrecta"
+                clase_mensaje = "card border-danger mb-3"
+        contexto = {
+            "mensaje" : mensaje,
+            "clase_mensaje" : clase_mensaje,
+        }
+        return render(request, "login/director_credenciales_propias.html", contexto)
+    else:
+        return render(request, "plantillas_base/base.html")
+
+def log_out(request):
+    logout(request)
+    return render(request, "plantillas_base/base.html")
