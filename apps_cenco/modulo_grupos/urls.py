b5b876c93bb23c491344ba805cce5d958713e804
Katherine <cm14066@ues.edu.sv>
2018-06-25 20:43:00 -0600

    Insertar alumno y encargado

--- apps_cenco/modulo_grupos/urls.py
+++ apps_cenco/modulo_grupos/urls.py
@@ -4,7 +4,8 @@ from apps_cenco.modulo_grupos import views
 
 urlpatterns = [
     url(r'^consultar/$', views.consultar_grupos),
-    url(r'^(?P<id_grupo>\d+)$', views.detalle_grupo, name="detalle_grupo")
+    url(r'^(?P<id_grupo>\d+)$', views.detalle_grupo, name="detalle_grupo"),
+    url(r'^asist_consultar/$',views.asist_consultar_grupos)
 
 ]
 
