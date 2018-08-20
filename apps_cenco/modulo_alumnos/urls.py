48cae42cf4993ada00e31afb862783db711820b4
roberto <roberto.ernesto.castro@gmail.com>
2018-08-19 18:54:59 -0600

    Correcciones en merge

--- apps_cenco/modulo_alumnos/urls.py
+++ apps_cenco/modulo_alumnos/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import url
 from apps_cenco.modulo_alumnos import views
 
 urlpatterns = [
-    url(r'^insertar/$', views.registro_alumno, name="InsertarAlumno"),
     url(r'^consultar/$', views.consultar_alumnos, name="asistenteConsultarAlumnos"),
     url(r'^(?P<id_alumno>\d+)$', views.detalle_alumno, name="detalle_alumno"),
-    url(r'^insertarEncargado/$', views.registrar_encargado, name="InsertarEncargado"),
     url(r'^modificar/(?P<id_alumno>\d+)$', views.modificar_alumno, name="modificar_alumno"),
     url(r'^misdatos/$', views.ver_alumno_propio, name="ver_alumno_propio"),
     url(r'^insertarAlum/$', views.inscribirAlumno, name="InsertarAlumnoV2"),
