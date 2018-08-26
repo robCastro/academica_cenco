55e4e9e15b9e4a0462409c60494efc0d56ca57c8
kevin <asdg>
2018-08-25 21:05:10 -0600

    incluye la app modulo_empleados y pantalla con la lista de empleados activos sin terminar.

--- academica_cenco/urls.py
+++ academica_cenco/urls.py
@@ -34,6 +34,8 @@ urlpatterns = [
     url(r'^alumnos/', include('apps_cenco.modulo_alumnos.urls')),
     url(r'^horarios/', include('apps_cenco.modulo_horarios.urls')),
     url(r'^configuracion/', include('apps_cenco.db_local.urls')),
+    url(r'^empleados/', include('apps_cenco.modulo_empleados.urls')),
+
 
     # for reset passwords
     url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'sesiones/password_reset_form.html',
