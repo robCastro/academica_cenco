6f6061183923c248113d69a33db0763316767be7
Katherine <cm14066@ues.edu.sv>
2018-09-08 20:17:59 -0600

    Merge branch 'Roberto' of https://gitlab.com/robCastro/academica_cenco
    # Conflicts:
    #	apps_cenco/modulo_alumnos/urls.py
    #	apps_cenco/modulo_alumnos/views.py

--- static/js/crear_encargado2.js
+++ static/js/crear_encargado2.js
@@ -96,7 +96,7 @@ $(document).on('submit', '#inscribirAlumno', function (a) {
             $('#btnGuardarAlumno').attr('disabled', false);
             $('#espereI').css('display', 'none');
             var m = $('#mensajeEmergente');
-            m.children('strong').html(response.responseText);
+            m.children('strong').html("Cambios Realizados!");
             m.css('display','block');
             m.css('opacity',1);
             m.removeClass();
