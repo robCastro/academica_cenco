40a270bf6b3e5b5fcd7f167db25ebe156df90fa0
Edwin Dimas <edwinenriquedimas@gmail.com>
2018-11-03 20:49:16 -0600

    Pantalla Crear pensum Primera Version.

--- apps_cenco/modulo_carrera/views.py
+++ apps_cenco/modulo_carrera/views.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from apps_cenco.db_app.models import Carrera
+from apps_cenco.db_app.models import Carrera, Materia, Cursa, DetallePensum
 from apps_cenco.modulo_carrera.forms import CrearEditarCarreraForm
+
 
 @login_required
 def crear_carrera(request):
@@ -26,6 +29,7 @@ def crear_carrera(request):
         context = {'form': form, 'editar': editar}
         return render(request,  'modulo_carrera/crear_editar_carrera.html', context)
 
+
 @login_required
 def editar_carrera(request, id_carrera):
     editar = True
@@ -42,4 +46,32 @@ def editar_carrera(request, id_carrera):
         form = CrearEditarCarreraForm(instance=carrera)
         context = {'form': form, 'editar': editar, 'id_carrera': id_carrera}
         return render(request, 'modulo_carrera/crear_editar_carrera.html', context)
+
+
+@login_required
+def crear_pensum(request, id_carrera):
+    detalle_pensumE = DetallePensum.objects.filter(carrera_id=id_carrera).distinct('carrera_id')
+    if not detalle_pensumE:
+        carrera = Carrera.objects.get(codigo_carrera=id_carrera)
+        if request.method == 'POST':
+            lista = request.POST.get('lista')
+            list_data = json.loads(lista)
+            for a in list_data:
+                materia = Materia.objects.get(codigo_materia=list_data[a])
+                detalle_pensum = DetallePensum(carrera=carrera, materia=materia, ordinal_materia_cursa=int(a))
+                detalle_pensum.save()
+            return HttpResponse('Pensum creado con exito.')
+        else:
+            materias = Materia.objects.all()
+            context = {'carrera': carrera, 'materias': materias}
+            return render(request, 'modulo_carrera/crear_pensum.html', context)
+    else:
+        context = {'existente': True}
+        return render(request, 'modulo_carrera/crear_pensum.html', context)
+
+
+@login_required
+def consultar_carrera(request):
+    # dummy
+    return 0
 
